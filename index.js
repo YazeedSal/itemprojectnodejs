424c3c47caa76bdb723084255bd289e8bd868092
ibrahims168 <ibrahh881@gmail.com>
2022-05-27 14:58:59 +0300

    mongodatabase

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const Item = require ('./server/models/item.model')
+
 mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("connected to database"))
 
 
@@ -14,9 +16,10 @@ app.get("/getItems", function (req,res){
    res.send(items) 
 })
 
-app.post("/additem", function (req,res){
-    const item = req.body
-    items.push(item)
+app.post("/additem", async function (req,res){
+    const item = new Item(req.body);
+    const response = await item.save();
+    res.send(response);
     res.send("item recieved")
 })
 
